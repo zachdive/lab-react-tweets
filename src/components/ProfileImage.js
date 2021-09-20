83a099c124711c6cf4a70d50e308b5a3e3d02221
Zach Dive <zjhd201@exeter.ac.uk>
2021-09-20 15:05:46 +0100

    ye boii

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,9 @@
+import React from  "react" ; 
+
+function ProfileImage({image}) { //=
+    
+    return <img src = {image} alt= "profilepic" className = "profile" />;
+    
+}
+
+export default ProfileImage;
---
+++ src/components/Timestamp.js
@@ -0,0 +1,14 @@
+import React from  "react" ; 
+
+function Timestamp({time}) { //=
+    
+    return <span className="timestamp">{time}</span>
+
+    
+}
+
+export default Timestamp;
+
+
+
+
